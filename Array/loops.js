60073b64a13350bdf7a713e0b8447d115b188220
SuryabhanVsn <suryabhanvsn.21@gmail.com>
2024-08-21 20:23:49 +0545

    message

---
+++ Array/loops.js
@@ -0,0 +1,14 @@
+// For_Loop
+
+/* let a = ['Avacado' , 'Dragon' , 'Kiwi' , 'Grapes'];
+for(i = 0; i < a.length; i++){
+    console.log(a[i]);
+} */
+
+// For-Of_Loop
+
+let b = ['Joe' , 'Smith' , 'Woakes' , 'Pat'];
+for (let test of b) {
+    b += test;
+    console.log(test);
+  }
