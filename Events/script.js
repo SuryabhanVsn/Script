c15cc151730c31a0c0d43e6293fdb77c52e9e73a
SuryabhanVsn <suryabhanvsn.21@gmail.com>
2024-08-24 09:53:08 +0545

    message

---
+++ Events/script.js
@@ -0,0 +1,11 @@
+function Click(){
+    console.log('choko ho rw');
+}
+
+function DBL_Click(){
+    console.log('2 palta choko ho rw');
+}
+
+function Hover(){
+    console.log('hover garya ho');
+}
